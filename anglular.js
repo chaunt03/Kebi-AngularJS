ba162be1be2e57b1f3b7f59242ab1ab552b621cc
Nguyen Thanh Chau <chauthanhnguyen838@gmail.com>
2024-04-03 19:48:31 +0700

    KebiAgularJS

---
+++ anglular.js
@@ -0,0 +1,415 @@
+var list = [
+    {
+        id: "giaycaogot",
+        name: "Giày Cao Gót Có Quai Cổ Chân - BMN 0309 - Màu Vàng Kim",
+        price: 425.000,
+        img: "Products/giaycaogot1.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Cao Gót Đính Nơ - BMN 0299 - Màu Đen",
+        price: 380.000,
+        img: "Products/giaycaogot2.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Sandal Cao Gót Quai Lượn Chéo- Màu Xanh Rêu",
+        price: 475.000,
+        img: "Products/giaycaogot3.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Cao Gót Phối Vải Tweed - BMN 0295 - Màu Đỏ Đậm",
+        price: 450.000,
+        img: "Products/giaycaogot4.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Cao Gót Ánh Kim Tuyến- Màu Bạc",
+        price: 380.000,
+        img: "Products/giaycaogot5.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Cao Gót Nhọn Bít Mũi - BMN 0312 - Màu Đen",
+        price: 425.000,
+        img: "Products/giaycaogot6.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Cao Gót Ánh Kim Tuyến - BMN 0330 - Màu Xám Khói Đậm",
+        price: 475.000,
+        img: "Products/giaycaogot7.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Cao Gót SDN 0569 - Màu Bạc",
+        price: 380.000,
+        img: "Products/giaycaogot8.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Cao Gót SDN 0591 - Màu Xám Khói Đậm",
+        price: 425.000,
+        img: "Products/giaycaogot9.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Sandal Cao Gót Quai Lượn Chéo - Màu Bạc",
+        price: 380.000,
+        img: "Products/giaycaogot10.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Bít Gót Trụ Lục Giác - Màu Xanh Navy - BMN",
+        price: 400.000,
+        img: "Products/giaycaogot11.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Sandal Gót Nhọn Họa Tiết Vân",
+        price: 349.000,
+        img: "Products/giaycaogot12.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Slingback Họa Tiết Hoa - BMN 0314",
+        price: 400.000,
+        img: "Products/giaycaogot13.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Bít Mũi Nhọn Gót Vuông - BMN 0326",
+        price: 369.000,
+        img: "Products/giaycaogot14.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Cao Gót D'Orsay Đính Đá - BMN 0317",
+        price: 349.000,
+        img: "Products/giaycaogot15.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Cao Gót Mary Janes Bản Lớn - BMN",
+        price: 380.000,
+        img: "Products/giaycaogot16.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Sandal Quai Chữ V Ánh Kim - SDN 0625 - Màu Nâu",
+        price: 349.000,
+        img: "Products/giaycaogot17.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Cao Gót Bít Mũi Nhọn- Màu Đen",
+        price: 369.000,
+        img: "Products/giaycaogot18.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Cao Quai Tròn - BMN 0324 - Màu Trắng",
+        price: 400.000,
+        img: "Products/giaycaogot19.jpg"
+    },
+    {
+        id: "giaycaogot",
+        name: "Giày Sandal Quai Chữ V Ánh Kim",
+        price: 349.000,
+        img: "Products/giaycaogot20.jpg"
+    },
+    {
+        id: "giaysandal",
+        name: "Giày Sandal Đế Bệt SDK 0279 - Màu Hoa Văn",
+        price: 425.000,
+        img: "Products/giaysandal1.jpg"
+    },
+    {
+        id: "giaysandal",
+        name: "Giày Sandal Đế Bệt SDK 0273 - Màu Vàng Kim",
+        price: 380.000,
+        img: "Products/giaysandal2.jpg"
+    },
+    {
+        id: "giaysandal",
+        name: "Giày Sandal Gót Vuông SDN 0598 - Màu Trắng",
+        price: 410.000,
+        img: "Products/giaysandal3.jpg"
+    },
+    {
+        id: "giaysandal",
+        name: "Giày Sandal Đế Bệt SDK 0279 - Màu Đen",
+        price: 425.000,
+        img: "Products/giaysandal4.jpg"
+    },
+    {
+        id: "giaysandal",
+        name: "Giày Sandal Đế Bệt Quai Ngang - Màu Be - SDK",
+        price: 380.000,
+        img: "Products/giaysandal5.jpg"
+    },
+    {
+        id: "giaysandal",
+        name: "Giày Sandal Quai Ngang - SDK 0282 - Màu Xanh",
+        price: 425.000,
+        img: "Products/giaysandal6.jpg"
+    },
+    {
+        id: "giaysandal",
+        name: "Giày Sandal Vân Da Rắn - SDN 0623 - Màu Đen",
+        price: 400.000,
+        img: "Products/giaysandal7.jpg"
+    },
+    {
+        id: "giaysandal",
+        name: "Giày Sandal Quai Ngang - SDX 0407 - Màu Xanh",
+        price: 380.000,
+        img: "Products/giaysandal8.jpg"
+    },
+    {
+        id: "giaysandal",
+        name: "Giày Sandal Đế Xuồng Ánh Kim - SDX 0398 - Màu Đen",
+        price: 425.000,
+        img: "Products/giaysandal9.jpg"
+    },
+    {
+        id: "giaysandal",
+        name: "Giày Sandal Gót Vuông Quai Cổ Chân - SDN",
+        price: 380.000,
+        img: "Products/giaysandal10.jpg"
+    },
+    {
+        id: "giaybb",
+        name: "Giày búp bê GBB 0378",
+        price: 250.000,
+        img: "Products/giaybb1.jpg"
+    },
+    {
+        id: "giaybb",
+        name: "Giày búp bê GBB 0374",
+        price: 250.000,
+        img: "Products/giaybb2.jpg"
+    },
+    {
+        id: "giaybb",
+        name: "Giày búp bê JNR 0069",
+        price: 325.000,
+        img: "Products/giaybb3.jpg"
+    },
+    {
+        id: "giaybb",
+        name: "Giày búp bê JNR 0083",
+        price: 275.000,
+        img: "Products/giaybb4.jpg"
+    },
+    {
+        id: "giaybb",
+        name: "Giày búp bê GBB 0394",
+        price: 250.000,
+        img: "Products/giaybb5.jpg"
+    },
+    {
+        id: "giaybb",
+        name: "Giày Búp Bê GBB 0396 - Màu Xanh Navy",
+        price: 275.000,
+        img: "Products/giaybb6.jpg"
+    },
+    {
+        id: "giaybb",
+        name: "Giày Búp Bê GBB 0396 - Màu Kem",
+        price: 325.000,
+        img: "Products/giaybb7.jpg"
+    },
+    {
+        id: "giaybb",
+        name: "Giày Búp Bê GBB- Màu Xanh Navy",
+        price: 275.000,
+        img: "Products/giaybb8.jpg"
+    },
+    {
+        id: "giaybb",
+        name: "Giày Búp Bê GBB 0401 - Màu Be",
+        price: 225.000,
+        img: "Products/giaybb9.jpg"
+    },
+    {
+        id: "giaybb",
+        name: "Giày Búp Bê Vải Tweed Đính Nơ - Màu Đen",
+        price: 250.000,
+        img: "Products/giaybb10.jpg"
+    },
+    {
+        id: "tuixach",
+        name: "Túi Xách Tay In Hoa - SAT 0226",
+        price: 780.000,
+        img: "Products/tuixach1.jpg"
+    },
+    {
+        id: "tuixach",
+        name: "Túi Cói Xách Tay Quai Tròn - SAT 0232",
+        price: 670.000,
+        img: "Products/tuixach2.jpg"
+    },
+    {
+        id: "tuixach",
+        name: "Túi Xách Quai Ánh Metallic",
+        price: 870.000,
+        img: "Products/tuixach3.jpg"
+    },
+    {
+        id: "tuixach",
+        name: "Túi xách tay TOT 0030",
+        price: 890.000,
+        img: "Products/tuixach4.jpg"
+    },
+    {
+        id: "tuixach",
+        name: "Túi xách da Balian",
+        price: 980.000,
+        img: "Products/tuixach5.jpg"
+    },
+    {
+        id: "tuixach",
+        name: "Túi xách trắng trơn",
+        price: 580.000,
+        img: "Products/tuixach6.jpg"
+    }
+]
+var app = angular.module("myapp", ["ngRoute"]);
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/home", {
+            templateUrl: "Home.html",
+        })
+        .when("/listproduct", {
+            templateUrl: "ListProduct.html",
+        })
+        .when("/detail/:id", {
+            templateUrl: "DetailPro.html",
+            controller: "detailCtrl"
+        })
+        .when("/login", {
+            templateUrl: "LogIn.html",
+        })
+        .when("/register", {
+            templateUrl: "Register.html"
+        })
+        .when("/cart", {
+            templateUrl: "ShoppingCart.html",
+            controller: "cartCtrl"
+        })
+        .when("/dosanpham/:id", {
+            templateUrl: "Dosanpham.html",
+            controller: "dosanphamCtrl"
+        })
+        .otherwise({
+            redirectTo: '/home',
+        })
+})
+
+app.controller("myctrl", function ($scope, $http) {
+    $scope.keyword = '';
+    $scope.productsTop10 = [];
+    $http.get("categoryTop10.json").then(function (res) {
+        $scope.productsTop10 = res.data;
+    })
+
+    $scope.productsNew = [];
+    $http.get("categoryNewProds.json").then(function (res) {
+        $scope.productsNew = res.data;
+    })
+
+    $scope.loai = []
+    $scope.dsSp = [];
+    $http.get('listproducts.json').then(function (res) {
+        $scope.dsSp = res.data;
+        // $scope.loai = res.data;
+
+        var a = $scope.dsSp.map(sp => sp.id)
+        $scope.loai = Array.from(new Set(a));
+        $scope.loai = $scope.loai.map(sp => ({
+            id: sp,
+            name: sp === "giaycaogot" ? "Giày cao gót" : sp === 'giaysandal' ? 'Giày sandal' :
+                sp === 'giaybb' ? 'Giày búp bê' : sp === 'tuixach' ? 'Túi xách' : ''
+        }))
+
+
+        $scope.page = 1;
+        $scope.limit = 8;
+        $scope.start = ($scope.page - 1) * $scope.limit;
+        $scope.totalPage = Math.ceil($scope.dsSp.length / $scope.limit);
+        $scope.numberOfPage = Array.from(Array($scope.totalPage).keys());
+
+        $scope.showPage = function (p) {
+            $scope.page = p;
+            $scope.start = ($scope.page - 1) * $scope.limit;
+        }
+    })
+
+
+    $scope.cot = '';
+    $scope.kieu = '';
+    $scope.order = function (cot, kieu) {
+        $scope.cot = cot;
+        $scope.kieu = kieu;
+    }
+
+    $scope.thongbao = '';
+    $scope.register = function () {
+        $scope.thongbao = "Đăng kí tài khoản thành công";
+        alert("Quay lại trang đăng nhập nhé!");
+    }
+});
+
+app.controller("dosanphamCtrl", function ($scope, $routeParams) {
+    $scope.id = $routeParams.id === undefined ? '' : $routeParams.id;
+    $scope.list = $scope.$parent.dsSp.filter(sp => sp.id === $scope.id)
+})
+
+app.controller('detailCtrl', function ($scope, $routeParams, $rootScope) {
+    $scope.name = $routeParams.id === undefined ? '' : $routeParams.id;
+    $scope.list = $scope.$parent.dsSp.find(sp => sp.name === $scope.name)
+    console.log($scope.list);
+    $rootScope.giohang = []
+    $scope.mua = function (prod) {
+        var chuaCo = true;
+        var prod = {
+            list: $scope.list,
+            soluong: $scope.soluong
+        }
+        for (const item of $rootScope.giohang) {
+            if (item.id == prod.id) {
+                //TH2: Đã có sp trong giỏ hàng
+                item.soluong++;
+                chuaCo = false;
+                break;
+            }
+        }
+        //TH1: Chưa có sản phẩm trong giỏ hàng
+        if (chuaCo) {
+            prod.soluong = 1;
+            $rootScope.giohang.push(prod);
+        }
+    }
+    console.log($rootScope.giohang)
+})
+
+app.controller('cartCtrl', function ($scope, $rootScope) {
+    $rootScope.tinhTongtt = function () {
+        var tong = 0;
+        for (const item of $rootScope.giohang) {
+            tong = tong + item.list.price * item.soluong;
+        }
+        return tong;
+    }
+    $scope.xoahet = function () {
+        $rootScope.giohang = [];
+    }
+    $scope.xoa = function (id) {
+        $rootScope.giohang = $rootScope.giohang.filter(prod => prod.id != id)
+    }
+})
+
+
+
